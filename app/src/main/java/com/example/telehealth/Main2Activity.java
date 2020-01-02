d251f3d6bd5534c88908721f17faf49a592d003c
MohammadQutit <MohammadQutit>
2020-01-02 16:08:57 +0200

    add title and logo

---
+++ app/src/main/java/com/example/telehealth/Main2Activity.java
@@ -0,0 +1,19 @@
+package com.example.telehealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.io.FileInputStream;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+
+    }
+}
